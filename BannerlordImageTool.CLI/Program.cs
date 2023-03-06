fd695fb1c91460e811d42e7c36eafead030eed49
Wizcas Chen <chen@0x1c.dev>
2023-03-07 00:26:16 +0800

    add the functionality of unpacking sprite from a sprite sheet

--- BannerlordImageTool.CLI/Program.cs
+++ BannerlordImageTool.CLI/Program.cs
@@ -1,19 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BannerlordImageTool.BannerTex;
+using BannerlordImageTool.Sprite;
 
 Console.WriteLine("Hello, World!");
 
-if (args.Length < 2)
+if (args.Length < 1)
 {
     Console.WriteLine("need more parameters.\n" +
-        "usage: BannerlordImageTool.CLI <outName> [srcFile1 srcFile2 ...]");
-
+        "usage: BannerlordImageTool.CLI <command> <arguments>");
+    return;
 }
-else
+
+Dictionary<string, Func<string[], int>> _cmdHandlers = new() {
+    {"bannerTex",DoBannerTex},
+    {"unpackSprite",UnpackSprite},
+};
+
+var cmd = args[0];
+if (!_cmdHandlers.TryGetValue(cmd, out var handler))
 {
+    var cmdList = string.Join((char)0, _cmdHandlers.Keys.Select(k => $"\n\t{k}"));
+    Console.WriteLine("unknown command.\navailable commands:" + cmdList);
+    return;
+}
+handler(args.Skip(1).ToArray());
+
+static int DoBannerTex(string[] args)
+{
+    /* Usage:
+     * bannerTex <outFile> [src1 [src2 ...]]
+     */
     var outName = args[0];
     var srcFiles = args.Skip(1).ToArray();
 
     var tm = new TextureMerger(OutputResolution.Res4K);
     tm.Merge(outName, srcFiles);
+    return 0;
+}
+
+static int UnpackSprite(string[] args)
+{
+    /* Usage: 
+     * BannerlordImageTool.CLI unpackSprite <spritesheet> <x,y,w,h> <outfile>
+     */
+    if (args.Length != 3)
+    {
+        Console.WriteLine($"usage: <spritesheet> <x,y,w,h> <outFile>");
+        return 1;
+    }
+    var unpacker = new SpriteUnpacker();
+    unpacker.UnpackSingle(args[0], args[2], SpriteRegion.FromString(args[1]));
+    return 0;
 }
---
+++ BannerlordImageTool.Sprite/SpriteUnpacker.cs
@@ -0,0 +1,45 @@
+﻿using ImageMagick;
+
+namespace BannerlordImageTool.Sprite;
+
+public class SpriteUnpacker
+{
+    public void UnpackSingle(string spriteSheet, string outputFile, SpriteRegion region)
+    {
+        if (string.IsNullOrEmpty(outputFile)) throw new ArgumentNullException("outputFile");
+        var settings = new MagickReadSettings() { ExtractArea = region.ToGeometry() };
+        using (var sprite = new MagickImage(spriteSheet, settings))
+        {
+            sprite.Write(outputFile);
+        }
+    }
+}
+public record SpriteRegion(int X, int Y, int Width, int Height)
+{
+    public static SpriteRegion FromString(string args)
+    {
+        var parts = args.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 4)
+        {
+            throw new ArgumentException("invalid sprite region. should be in the format of x,y,width,height");
+        }
+        if (!int.TryParse(parts[0], out var x))
+        {
+            throw new ArgumentException($"invalid sprite x: {parts[0]}");
+        }
+        if (!int.TryParse(parts[1], out var y))
+        {
+            throw new ArgumentException($"invalid sprite y: {parts[1]}");
+        }
+        if (!int.TryParse(parts[2], out var w))
+        {
+            throw new ArgumentException($"invalid sprite w: {parts[2]}");
+        }
+        if (!int.TryParse(parts[3], out var h))
+        {
+            throw new ArgumentException($"invalid sprite h: {parts[3]}");
+        }
+        return new SpriteRegion(x, y, w, h);
+    }
+    public MagickGeometry ToGeometry() => new MagickGeometry(X, Y, Width, Height);
+}
