c38b666b5e1a1b014b86a86221e295d7a89f2666
Wizcas Chen <chen@0x1c.dev>
2023-03-19 04:57:49 +0800

    add the group list and corresponding vm. [wip] group editor is not fully updated when switching to another group

---
+++ BannerlordImageTool.Win/Pages/BannerIcons/BannerIconGroupEditor.xaml.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System.Linq;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using BannerlordImageTool.Win.ViewModels.BannerIcons;
+using BannerlordImageTool.Win.Common;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BannerlordImageTool.Win.Pages.BannerIcons;
+
+public sealed partial class BannerIconGroupEditor : UserControl
+{
+    public GroupViewModel ViewModel
+    {
+        get => GetValue(ViewModelProperty) as GroupViewModel;
+        set => SetValue(ViewModelProperty, value);
+    }
+    public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+        nameof(ViewModel),
+        typeof(GroupViewModel),
+        typeof(BannerIconsEditor),
+        new PropertyMetadata(default(GroupViewModel)));
+
+    public BannerIconGroupEditor()
+    {
+        this.InitializeComponent();
+        ViewModel = new GroupViewModel();
+    }
+
+    private void btnConfirmDelete_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.DeleteIcons(ViewModel.Selection);
+        flyoutConfirmDelete.Hide();
+    }
+
+    async void btnOpenImages_Click(object sender, RoutedEventArgs e)
+    {
+        var files = await FileHelper.PickMultipleFiles(".png");
+
+        if (files.Count == 0) return;
+        ViewModel.AddIcons(files);
+    }
+
+    private void GridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var changed = false;
+        foreach (var item in e.AddedItems.Where(item => item is IconViewModel).Cast<IconViewModel>())
+        {
+            item.IsSelected = true;
+            changed = true;
+        }
+        foreach (var item in e.RemovedItems.Where(item => item is IconViewModel).Cast<IconViewModel>())
+        {
+            item.IsSelected = false;
+            changed = true;
+        }
+        if (changed)
+        {
+            ViewModel.NotifySelectionChange();
+        }
+    }
+}
