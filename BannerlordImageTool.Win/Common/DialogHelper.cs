1420f85fd0f05b67b732944ec6992c204e144dfd
Wizcas Chen <chen@0x1c.dev>
2023-03-21 23:23:45 +0800

    implement the grid inline color picker

--- BannerlordImageTool.Win/Common/DialogHelper.cs
+++ BannerlordImageTool.Win/Common/DialogHelper.cs
@@ -12,18 +12,21 @@ public class DialogHelper
 {
     public static Task<ContentDialogResult> ShowDangerConfirmDialog(UIElement sender, string title, string content)
     {
-        var dialog = new ContentDialog() {
+        var dialog = GetBaseDialog(sender);
+        dialog.Title = title;
+        dialog.PrimaryButtonStyle = Application.Current.Resources["DangerButton"] as Style;
+        dialog.PrimaryButtonText = I18n.Current.GetString("Yes");
+        dialog.SecondaryButtonText = I18n.Current.GetString("No");
+        dialog.DefaultButton = ContentDialogButton.Secondary;
+        dialog.Content = new TextBlock() { Text = content };
+        return dialog.ShowAsync().AsTask();
+    }
+
+    public static ContentDialog GetBaseDialog(UIElement sender)
+    {
+        return new ContentDialog() {
             XamlRoot = sender.XamlRoot,
             Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
-            Title = title,
-            PrimaryButtonStyle = Application.Current.Resources["DangerButton"] as Style,
-            PrimaryButtonText = I18n.Current.GetString("Yes"),
-            SecondaryButtonText = I18n.Current.GetString("No"),
-            DefaultButton = ContentDialogButton.Secondary,
-            Content = new TextBlock() {
-                Text = content,
-            }
         };
-        return dialog.ShowAsync().AsTask();
     }
 }
--- BannerlordImageTool.Win/Pages/BannerIcons/BannerColorsEditor.xaml.cs
+++ BannerlordImageTool.Win/Pages/BannerIcons/BannerColorsEditor.xaml.cs
@@ -18,6 +18,7 @@ using Microsoft.UI.Xaml.Navigation;
 using System.Collections.ObjectModel;
 using BannerlordImageTool.Win.ViewModels.BannerIcons;
 using Windows.UI;
+using BannerlordImageTool.Win.Common;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,8 +31,27 @@ public sealed partial class BannerColorsEditor : UserControl
             new ColorViewModel(){ID=123,Color=Color.FromArgb(255,255,0,0)},
             new ColorViewModel(){ID=234,Color=Color.FromArgb(255,0,255,0)},
         };
+
     public BannerColorsEditor()
     {
         this.InitializeComponent();
+    }
+
+    private async void btnChangeColor_Click(object sender, RoutedEventArgs e)
+    {
+        var tag = (sender as Button)?.Tag;
+        if (tag is null || tag is not ColorViewModel vm) return;
+        var dialog = DialogHelper.GetBaseDialog(this);
+        dialog.Title = I18n.Current.GetString("DialogSelectColor/Title");
+        dialog.PrimaryButtonText = I18n.Current.GetString("ButtonOK/Content");
+        dialog.SecondaryButtonText = I18n.Current.GetString("ButtonCancel/Content");
+        dialog.DefaultButton = ContentDialogButton.Primary;
+        var colorPicker = new ColorPicker() { Color = vm.Color };
+        dialog.Content = colorPicker;
+        var result = await dialog.ShowAsync().AsTask();
+        if (result == ContentDialogResult.Primary)
+        {
+            vm.Color = colorPicker.Color;
+        }
     }
 }
