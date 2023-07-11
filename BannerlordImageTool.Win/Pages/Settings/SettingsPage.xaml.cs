dbfbab48e4a7c98737bd62a93f62344153722b83
Wizcas Chen <chen@0x1c.dev>
2023-07-12 01:43:16 +0800

    change language in Settings

--- BannerlordImageTool.Win/Pages/Settings/SettingsPage.xaml.cs
+++ BannerlordImageTool.Win/Pages/Settings/SettingsPage.xaml.cs
@@ -3,7 +3,12 @@
 
 using BannerlordImageTool.Win.Helpers;
 using BannerlordImageTool.Win.Pages.Settings.ViewModels;
+using BannerlordImageTool.Win.Services;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.AppLifecycle;
+using System;
+using System.Linq;
+using Windows.Globalization;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -15,6 +20,10 @@ namespace BannerlordImageTool.Win.Pages.Settings;
 /// </summary>
 public sealed partial class SettingsPage : Page
 {
+    public readonly Tuple<string, string>[] Languages = new[] {
+        new Tuple<string, string>("English", "en"),
+        new Tuple<string, string>("¼òÌåÖÐÎÄ", "zh"),
+    };
     SettingsViewModel ViewModel { get; } = new();
 
     public SettingsPage()
@@ -26,4 +35,35 @@ public sealed partial class SettingsPage : Page
     {
         FileHelpers.OpenFolderInExplorer(Logging.Folder);
     }
+
+    async void cboLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var old = (e.RemovedItems.FirstOrDefault() as Tuple<string, string>)?.Item2;
+        var selected = (e.AddedItems.FirstOrDefault() as Tuple<string, string>)?.Item2;
+        if (selected == CurrentLang || CurrentLang.StartsWith(selected)) { return; }
+        ApplicationLanguages.PrimaryLanguageOverride = selected;
+        ContentDialogResult result = await AppServices.Get<IConfirmDialogService>().ShowWarn(this,
+            I18n.Current.GetString("DialogChangeLanguage/Title"),
+            I18n.Current.GetString("DialogChangeLanguage/Content")
+            );
+        if (result == ContentDialogResult.Primary)
+        {
+            // FIXME: WinUI3 seems not able to change language at runtime. So I have to restart the app as a workaround.
+            //        See: https://github.com/microsoft/microsoft-ui-xaml/issues/5940
+            AppInstance.Restart("");
+        }
+    }
+
+    void cboLanguage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var current = CurrentLang;
+        Tuple<string, string> item = Languages.FirstOrDefault(item => item.Item2 == current);
+        if (item == null)
+        {
+            item = Languages.FirstOrDefault(item => current.StartsWith(item.Item2));
+        }
+        cboLanguage.SelectedItem = item;
+    }
+
+    string CurrentLang { get => Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse().Languages[0]; }
 }
--- BannerlordImageTool.Win/Services/ConfirmDialogService.cs
+++ BannerlordImageTool.Win/Services/ConfirmDialogService.cs
@@ -10,6 +10,7 @@ public interface IConfirmDialogService
 {
     ContentDialog Create(UIElement sender);
     Task<ContentDialogResult> Show(UIElement sender, Action<ContentDialog> customizer);
+    Task<ContentDialogResult> ShowWarn(UIElement sender, string title, string content);
     Task<ContentDialogResult> ShowDanger(UIElement sender, string title, string content);
 }
 
@@ -27,6 +28,17 @@ public class ConfirmDialogService : IConfirmDialogService
         ContentDialog dialog = Create(sender);
         customizer(dialog);
         return dialog.ShowAsync().AsTask();
+    }
+    public Task<ContentDialogResult> ShowWarn(UIElement sender, string title, string content)
+    {
+        return Show(sender, (dialog) => {
+            dialog.Title = title;
+            dialog.PrimaryButtonStyle = Application.Current.Resources["WarnButton"] as Style;
+            dialog.PrimaryButtonText = I18n.Current.GetString("OK");
+            dialog.SecondaryButtonText = I18n.Current.GetString("Cancel");
+            dialog.DefaultButton = ContentDialogButton.Secondary;
+            dialog.Content = new TextBlock() { Text = content };
+        });
     }
     public Task<ContentDialogResult> ShowDanger(UIElement sender, string title, string content)
     {
