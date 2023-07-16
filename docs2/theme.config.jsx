93e09ac08d9d6e662df8dda45912a2aeca4e26a9
Wizcas Chen <chen@0x1c.dev>
2023-07-17 02:11:03 +0800

    i18n settings

--- docs2/theme.config.jsx
+++ docs2/theme.config.jsx
@@ -1,6 +1,15 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: (
+    <>
+      <img src="/logo/icon.png" alt="logo" width={32} style={{ marginRight: "0.5em" }} />
+      <span>BLIT</span>
+    </>
+  ),
   project: {
     link: "https://github.com/0x1c-works/BLIT",
   },
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "zh", text: "中文" },
+  ],
 };
