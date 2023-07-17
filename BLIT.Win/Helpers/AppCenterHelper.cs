a7f42fb604eee3f4628ff92d0998b5d2525852e2
Wizcas Chen <chen@0x1c.dev>
2023-07-18 03:09:42 +0800

    integrated the AppCenter

---
+++ BLIT.Win/Helpers/AppCenterHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BLIT.Win.Helpers;
+class AppCenterHelper
+{
+    const string APP_CENTER_SECRET = "BLIT_APP_CENTER_SECRET";
+    public static string Secret
+    {
+        get => Environment.GetEnvironmentVariable(APP_CENTER_SECRET, EnvironmentVariableTarget.User);
+    }
+}
