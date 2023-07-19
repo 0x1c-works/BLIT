15ff0ca42036e3e62890db229da292579774e30b
Wizcas Chen <chen@0x1c.dev>
2023-07-20 05:18:07 +0800

    toolbar style tweaking

--- BLIT/ViewModels/BannerIconsProjectViewModel.cs
+++ BLIT/ViewModels/BannerIconsProjectViewModel.cs
@@ -4,6 +4,7 @@ using Wpf.Ui.Common.Interfaces;
 namespace BLIT.ViewModels;
 public class BannerIconsProjectViewModel : ObservableObject, INavigationAware
 {
+    public string[] TextureResolutions = new[] { "2K", "4K" };
     public void OnNavigatedFrom()
     {
     }
