6f5e144ae9e5fc751f48b2a177c1c01080c8b300
Wizcas Chen <chen@0x1c.dev>
2023-07-24 04:21:44 +0800

    BannerSettingsSection layout

---
+++ BLIT/Views/Settings/BannerSettingsSection.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace BLIT.Views.Settings;
+
+/// <summary>
+/// Interaction logic for BannerSettingsSection.xaml
+/// </summary>
+public partial class BannerSettingsSection : UserControl
+{
+    public BannerSettingsSection()
+    {
+        InitializeComponent();
+    }
+}
