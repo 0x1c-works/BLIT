2aa91cf803c3078d2cf9f36a17a3b8e685f757a0
Wizcas Chen <chen@0x1c.dev>
2024-01-27 21:07:39 +0800

    finish the settings page

--- BLIT/scripts/UI/Settings/SettingsPage.cs
+++ BLIT/scripts/UI/Settings/SettingsPage.cs
@@ -1,33 +1,41 @@
-﻿using Godot;
+﻿using BLIT.scripts.Common;
+using Godot;
 
 public partial class SettingsPage : Control {
     public static readonly (string, string)[] LANGS = [
         ("en", "English"),
         ("zh", "中文"),
     ];
-    [Export] public OptionButton? Language { get; set; }
+    [Export] public OptionButton? LanguageSelect { get; set; }
+    [Export] public Label? VersionLabel { get; set; }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
-        if (Language != null) {
+        if (LanguageSelect != null) {
             var currentLocale = TranslationServer.GetLocale();
-            Language.Clear();
+            LanguageSelect.Clear();
             var selectedIndex = -1;
             for (var i = 0; i < LANGS.Length; i++) {
                 (string, string) lang = LANGS[i];
-                Language.AddItem(lang.Item2, i);
-                Language.SetItemMetadata(i, lang.Item1);
+                LanguageSelect.AddItem(lang.Item2, i);
+                LanguageSelect.SetItemMetadata(i, lang.Item1);
                 if (currentLocale.StartsWith(lang.Item1)) {
                     selectedIndex = i;
                 }
             }
-            Language.Select(selectedIndex);
-            Language.ItemSelected += OnLanguageSelected;
+            LanguageSelect.Select(selectedIndex);
+            LanguageSelect.ItemSelected += OnLanguageSelected;
+        }
+        if (VersionLabel != null) {
+            VersionLabel.Text = (string)ProjectSettings.GetSetting("application/config/version");
         }
     }
 
     private void OnLanguageSelected(long index) {
-        TranslationServer.SetLocale((string)(Language?.GetItemMetadata((int)index))!);
+        TranslationServer.SetLocale((string)(LanguageSelect?.GetItemMetadata((int)index))!);
+    }
+    private void OnOpenLog() {
+        OS.ShellShowInFileManager(Logging.Folder, true);
     }
 
 }
