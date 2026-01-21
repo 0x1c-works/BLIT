b5df74f8a7c2de38b45c69861e5cf36f5e32ee05
Wizcas Chen <chen@0x1c.dev>
2026-01-21 20:53:48 +0800

    optimize with virtual list

--- BLIT.WPF/Helpers/BindingConverters.cs
+++ BLIT.WPF/Helpers/BindingConverters.cs
@@ -1,12 +1,48 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace BLIT.WPF.Helpers;
 
 public class InvertBoolConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         return value is bool b ? !b : value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
+
+public class PathToOptimizedBitmapImageConverter : IValueConverter {
+    /// <summary>
+    /// Converts a file path to an optimized BitmapImage with specified decode pixel width.
+    /// Parameter should be the DecodePixelWidth value (e.g., "128", "256", "512")
+    /// </summary>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is not string path || string.IsNullOrEmpty(path)) {
+            return null!;
+        }
+
+        if (!int.TryParse(parameter?.ToString() ?? "128", out int decodePixelWidth)) {
+            decodePixelWidth = 128;
+        }
+
+        try {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(path, UriKind.Absolute);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.DecodePixelWidth = decodePixelWidth;
+            bitmap.EndInit();
+            bitmap.Freeze();
+            return bitmap;
+        }
+        catch {
+            // Return null if path is invalid or image cannot be loaded
+            return null!;
+        }
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
