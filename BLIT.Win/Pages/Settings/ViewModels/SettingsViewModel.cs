0ee21af653898ee69f294a8556e9593d8e149bcc
Wizcas Chen <chen@0x1c.dev>
2026-01-30 05:53:41 +0800

    delete BLIT.Win

--- BLIT.Win/Pages/Settings/ViewModels/SettingsViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿using BLIT.Win.Helpers;
-
-namespace BLIT.Win.Pages.Settings.ViewModels;
-
-public class SettingsViewModel : BindableBase {
-    public BannerSettingsViewModel BannerSettings { get; } = new();
-}
